212e6e9fed324d82d39baa0c838753c35e75e67d
0Miniflyer0 <145483845+0Miniflyer0@users.noreply.github.com>
2026-09-14 11:13:48 -0400

    Added user model, Database, and Password Hashing For Signup

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'some-secret-key'
@@ -22,10 +24,19 @@ def signup():
             flash("Passwords do not match!")
             return redirect(url_for('signup'))
 
-        print(f"Recieved signup: {username}, {password}, {confirm_password}")
+        new_user = User(username=username, password_hash=generate_password_hash(password))
+        db.session.add(new_user)
+        db.session.commit()
         return render_template('signup_success.html')
     return render_template('signup.html')
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
